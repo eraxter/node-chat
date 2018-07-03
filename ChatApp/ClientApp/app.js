dbec91c519c8b049e9431c1207bd016d0b3c9273
Erik Raxter <erikrax@gmail.com>
2018-07-03 00:32:10 -0400

    changed ChatApp project type

--- ChatApp/ClientApp/app.js
+++ ChatApp/ClientApp/app.js
@@ -2,24 +2,23 @@
     'use strict';
 
     angular
-        .module('nodeChat', ['ngRoute'])
+        .module('socketChat', ['ngRoute'])
         .config(function ($routeProvider) {
             $routeProvider
                 .when('/', {
                     controller: 'MainController as main',
-                    templateUrl: '../Content/templates/main.html'
+                    templateUrl: 'ClientApp/templates/main.html'
                 })
                 .when('/chat', {
                     controller: 'ChatController as chat',
-                    templateUrl: '../Content/templates/chat.html'
+                    templateUrl: 'ClientApp/templates/chat.html'
                 })
                 .otherwise({ redirectTo: '/' });
         })
-        .factory('socket', function () {
+        .factory('$socket', function () {
             return new Socket('localhost:8081', { secure: true });
         })
-        .factory('user', function () {
+        .factory('$user', function () {
             return { id: '', name: '', room: 'test' };
         });
-
 })();
---
+++ ChatApp/ClientApp/main.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('socketChat')
+        .controller('MainController', MainController);
+
+    MainController.$inject = ['$location', '$socket', '$user'];
+
+    function MainController($location, $socket, $user) {
+
+        var self = this;
+
+        self.user = $user;
+
+        self.connect = function () {
+            $socket.options.query = 'name=' + self.user.name + '&room=' + self.user.room;
+            $socket.open(function () {
+                $location.path('/chat');
+            });
+        };
+    }
+})();
