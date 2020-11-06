ca9e42c8db026d7053349a61526ff896c6b25a4f
Erik Raxter <erikrax@gmail.com>
2020-11-06 00:45:09 -0500

    Updated the chat client to a NET Framework MVC application; Updated socket.io to version 2.3.0; Updated jquery to version 3.5.1; Updated angular to version 1.8.0; Updated popper to version 1.16.1; Updated boostrap to version 4.5.3;

---
+++ ChatApp/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace ChatApp
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/scripts/bootstrap").Include(
+                "~/Scripts/jquery-3.5.1.min.js",
+                "~/Scripts/umd/popper.min.js",
+                "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts/angular").Include(
+                "~/Scripts/angular.min.js",
+                "~/Scripts/angular-route.min.js"));
+
+            bundles.Add(new ScriptBundle("~/scripts/app").Include(
+                "~/Scripts/chat/socket.js",
+                "~/Scripts/chat/app.js",
+                "~/Scripts/chat/main.js",
+                "~/Scripts/chat/chat.js"));
+
+            bundles.Add(new StyleBundle("~/styles/main").Include(
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/main.css"));
+        }
+    }
+}
---
+++ ChatApp/Scripts/chat/app.js
@@ -0,0 +1,21 @@
+﻿'use strict';
+angular
+    .module('socketChat', ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'MainController',
+                templateUrl: '/Content/html/main.html'
+            })
+            .when('/chat', {
+                controller: 'ChatController',
+                templateUrl: '/Content/html/chat.html'
+            })
+            .otherwise({ redirectTo: '/' });
+    })
+    .factory('$socket', function () {
+        return new Socket(window.location.hostname, { secure: true });
+    })
+    .factory('$user', function () {
+        return { id: '', name: '', room: 'testing' };
+    });
