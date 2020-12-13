2cd4b817f4478ecb383e17a750d96b9ddc05f504
Erik Raxter <erikrax@gmail.com>
2020-12-13 10:20:20 -0500

    update socket.io to version 3.0.4

--- ChatServer/server.js
+++ ChatServer/server.js
@@ -4,7 +4,10 @@ var port = 8081;
 
 var options = {
     transports: ['polling', 'websocket'],
-    pingInterval: 10000
+    pingInterval: 10000,
+    cors: {
+        origin: 'https://localhost:44387'
+    }
 };
 
 var server = require('https').createServer({
@@ -19,44 +22,45 @@ var clients = [];
 var getClients = function (room) {
     var ca = clients;
     if (room) {
-        ca = clients.filter(function (client) {
-            return room === client.handshake.query.room;
+        ca = clients.filter(function (socket) {
+            return socket.rooms.has(room);
         });
     }
-    return ca.map(function (client) {
-        return { id: client.id, name: client.handshake.query.name };
+    return ca.map(function (socket) {
+        return { id: socket.id, name: socket.handshake.query.name };
     });
 };
 
-io.on('connection', function (client) {
-    var name = client.handshake.query.name;
-    var room = client.handshake.query.room;
+io.on('connection', function (socket) {
+    var name = socket.handshake.query.name;
+    var room = socket.handshake.query.room;
 
     if (!name || !room) {
-        client.disconnect(true);
+        socket.disconnect(true);
     }
     else {
-        clients.push(client);
+        clients.push(socket);
 
-        client.on('disconnect', function () {
-            clients.splice(clients.indexOf(client), 1);
-            client.to(room).emit('message', { text: name + ' has left the chat.' });
+        socket.on('disconnect', function () {
+            clients.splice(clients.indexOf(socket), 1);
+            socket.leave(room);
+            io.to(room).emit('message', { text: name + ' has left the chat' });
+
             var users = getClients(room);
-            client.to(room).emit('users', users);
+            io.to(room).emit('users', users);
         });
 
-        client.on('message', function (message) {
+        socket.on('message', function (message) {
             if (message && message.to) {
-                client.to(message.to).emit('message', message);
+                socket.to(message.to).emit('message', message);
             }
         });
 
-        client.join(room, function () {
-            client.to(room).emit('message', { text: name + ' has joined the chat.' });
-            var users = getClients(room);
-            client.to(room).emit('users', users);
-            client.emit('users', users);
-        });
+        socket.join(room);
+        socket.to(room).emit('message', { text: name + ' has joined the chat' });
+
+        var users = getClients(room);
+        io.to(room).emit('users', users);
     }
 });
 
