98c5ace22811cbf3f5f0e7e529f47722237cb01e
Erik Raxter <erikrax@gmail.com>
2020-12-14 06:56:42 -0500

    update server.js

--- ChatServer/server.js
+++ ChatServer/server.js
@@ -1,24 +1,19 @@
 'use strict';
 var fs = require('fs');
 var port = 8081;
-
 var options = {
     transports: ['polling', 'websocket'],
     pingInterval: 10000,
     cors: {
-        origin: 'https://localhost:44387'
+        origin: 'https://localhost:44325'
     }
 };
-
 var server = require('https').createServer({
     cert: fs.readFileSync('C:/certs/cert.crt'),
     key: fs.readFileSync('C:/certs/key.pem')
 });
-
 var io = require('socket.io')(server, options);
-
 var clients = [];
-
 var getClients = function (room) {
     var ca = clients;
     if (room) {
@@ -37,32 +32,27 @@ io.on('connection', function (socket) {
 
     if (!name || !room) {
         socket.disconnect(true);
+        return;
     }
-    else {
-        clients.push(socket);
 
-        socket.on('disconnect', function () {
-            clients.splice(clients.indexOf(socket), 1);
-            socket.leave(room);
-            io.to(room).emit('message', { text: name + ' has left the chat' });
+    clients.push(socket);
 
-            var users = getClients(room);
-            io.to(room).emit('users', users);
-        });
+    socket.on('disconnect', function () {
+        clients.splice(clients.indexOf(socket), 1);
+        socket.to(room).emit('message', { text: name + ' has left the chat' });
+        socket.leave(room);
+        io.to(room).emit('users', getClients(room));
+    });
 
-        socket.on('message', function (message) {
-            if (message && message.to) {
-                socket.to(message.to).emit('message', message);
-            }
-        });
+    socket.on('message', function (message) {
+        if (message && message.to) {
+            socket.to(message.to).emit('message', message);
+        }
+    });
 
-        socket.join(room);
-        socket.to(room).emit('message', { text: name + ' has joined the chat' });
-
-        var users = getClients(room);
-        io.to(room).emit('users', users);
-    }
+    socket.join(room);
+    socket.to(room).emit('message', { text: name + ' has joined the chat' });
+    io.to(room).emit('users', getClients(room));
 });
-
 server.listen(port);
 console.log('server is listening on port ' + port);
